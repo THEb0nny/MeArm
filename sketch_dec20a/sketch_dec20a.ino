b965b5b4a93702a78b6835be2c1237e43cfc49e0
Dmitriy Antipov <aston_bonny@mail.ru>
2022-03-11 22:57:35 +0300

    11_03_22_2

--- sketch_dec20a/sketch_dec20a.ino
+++ sketch_dec20a/sketch_dec20a.ino
@@ -6,17 +6,14 @@
 #include <ServoSmooth.h>
 #include "GyverFilters.h"
 
-#define AMOUNT_SERVO 2 // Количество серво
+#define SERVO_AMOUNT 2 // Количество серво
 #define POT1_PIN A0
 #define POT2_PIN A1
 #define SERVO1_PIN 9
 #define SERVO2_PIN 10
 
-ServoSmooth servos[AMOUNT_SERVO];
+ServoSmooth servos[SERVO_AMOUNT];
 GFilterRA analogGFilterRA;
-
-uint32_t servoTimer;
-boolean flag;
 
 void setup() {
   Serial.begin(9600);
@@ -29,17 +26,19 @@ void setup() {
   servos[1].setSpeed(130);
   servos[0].setAccel(0.1); // Установить ускорение (разгон и торможение)
   servos[1].setAccel(0.1);
-  analogGFilterRA.setCoef(0.01); // установка коэффициента фильтрации (0.0... 1.0). Чем меньше, тем плавнее фильтр
-  analogGFilterRA.setStep(10); // установка шага фильтрации (мс). Чем меньше, тем резче фильтр
+  analogGFilterRA.setCoef(0.01); // Установка коэффициента фильтрации (0.0... 1.0). Чем меньше, тем плавнее фильтр
+  analogGFilterRA.setStep(10); // Установка шага фильтрации (мс). Чем меньше, тем резче фильтр
 }
 
 void loop() {
   Serial.println(analogGFilterRA.filteredTime(analogRead(POT1_PIN)));
-  if (millis() - servoTimer >= 3000) { // Каждые 3 сек
+  /*if (millis() - servoTimer >= 3000) { // Каждые 3 сек
     servoTimer = millis();
-    flag = !flag;
-    servos[0].setTargetDeg(flag ? 50 : 120);  // Двигаем на углы 50 и 120
-  }
+    servos[0].setTargetDeg(flag ? 0 : 180);  // Двигаем на углы 50 и 120
+  }*/
+  servos[0].setTargetDeg(120);  // Двигаем на углы 50 и 120
+  while (servos[0].getCurrent() == 0); // Пока сервопривод двигается
+  servos[0].setTargetDeg(0);
   /*
   int pot1Value = analogRead(POT1_PIN);
   int servo1Pos = map(pot1Value, 0, 1023, 0, 180);
