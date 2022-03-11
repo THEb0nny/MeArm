e80ed6d931735322c23c38b881bc104aad2b393a
Dmitriy Antipov <aston_bonny@mail.ru>
2022-03-11 15:16:12 +0300

    11_03_22

--- sketch_dec20a/sketch_dec20a.ino
+++ sketch_dec20a/sketch_dec20a.ino
@@ -1,20 +1,21 @@
 // https://alexgyver.ru/servosmooth/
+// https://alexgyver.ru/lessons/filters/
+// https://alexgyver.ru/gyverfilters/
 
-//#include <Servo.h>
-#include <ServoSmooth.h>
 #include <SoftwareSerial.h>
+#include <ServoSmooth.h>
+#include "GyverFilters.h"
 
 #define AMOUNT_SERVO 2 // Количество серво
-
 #define POT1_PIN A0
 #define POT2_PIN A1
 #define SERVO1_PIN 9
 #define SERVO2_PIN 10
 
-//Servo servo1, servo2;
 ServoSmooth servos[AMOUNT_SERVO];
+GFilterRA analogGFilterRA;
 
-uint32_t timer;
+uint32_t servoTimer;
 boolean flag;
 
 void setup() {
@@ -22,25 +23,22 @@ void setup() {
   // Настройка выходов/выходов
   pinMode(POT1_PIN, INPUT);
   pinMode(POT2_PIN, INPUT);
-  //servo1.attach(SERVO1_PIN);
-  //servo2.attach(SERVO2_PIN);
-  servos[0].attach(POT1_PIN);
-  servos[1].attach(POT2_PIN);
-  servos[0].setSpeed(130); // ограничить скорость
+  servos[0].attach(POT1_PIN, 600, 2400);
+  servos[1].attach(POT2_PIN, 600, 2400);
+  servos[0].setSpeed(130); // Ограничить скорость
   servos[1].setSpeed(130);
-  servos[0].setAccel(0.1);       // установить ускорение (разгон и торможение)
+  servos[0].setAccel(0.1); // Установить ускорение (разгон и торможение)
   servos[1].setAccel(0.1);
-  //servo1.write(90);
-  //servo2.write(90);
+  analogGFilterRA.setCoef(0.01); // установка коэффициента фильтрации (0.0... 1.0). Чем меньше, тем плавнее фильтр
+  analogGFilterRA.setStep(10); // установка шага фильтрации (мс). Чем меньше, тем резче фильтр
 }
 
 void loop() {
-  servo.tick();
-
-  if (millis() - timer >= 3000) {   // каждые 3 сек
-    timer = millis();
+  Serial.println(analogGFilterRA.filteredTime(analogRead(POT1_PIN)));
+  if (millis() - servoTimer >= 3000) { // Каждые 3 сек
+    servoTimer = millis();
     flag = !flag;
-    servo[0].setTargetDeg(flag ? 50 : 120);  // двигаем на углы 50 и 120
+    servos[0].setTargetDeg(flag ? 50 : 120);  // Двигаем на углы 50 и 120
   }
   /*
   int pot1Value = analogRead(POT1_PIN);
